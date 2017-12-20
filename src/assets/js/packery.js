c5f5e2ce572577c8f6b7c6233391bcd32cff8697
Po Chun, Lu <oceanus11034@gmail.com>
2017-12-20 22:14:48 +0800

    [Update] Make Packery use local variables

--- src/assets/js/packery.js
+++ src/assets/js/packery.js
@@ -2,23 +2,26 @@ import Packery from 'packery';
 import Draggabilly from 'draggabilly';
 
 (() => {
-  const elem = document.querySelector('.grid');
-  const pckry = new Packery(elem, {
-    // options
-    itemSelector: '.grid-item',
-    gutter: 0,
-  });
+  // Make cards Draggable & display with masonry layout
+  function initCardsLayout() {
+    const cardsList = document.querySelector('.grid');
+    const pckry = new Packery(cardsList, {
+      itemSelector: '.grid-item',
+      gutter: 0,
+    });
 
-  function makeItemDraggable(itemElem) {
-    // make element draggable with Draggabilly
-    const draggie = new Draggabilly(itemElem);
-    // bind Draggabilly events to Packery
-    pckry.bindDraggabillyEvents(draggie);
+    function makeItemDraggable(itemElem) {
+      // make element draggable with Draggabilly
+      const draggie = new Draggabilly(itemElem);
+      // bind Draggabilly events to Packery
+      pckry.bindDraggabillyEvents(draggie);
+    }
+
+    // bind draggabilly events to item elements
+    pckry.items.forEach((item) => {
+      makeItemDraggable(item.element);
+    });
   }
-
-  // bind draggabilly events to item elements
-  pckry.items.forEach((item) => {
-    makeItemDraggable(item.element);
-  });
+  initCardsLayout();
 }
 )();
