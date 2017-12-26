cf724f54742fc5fb41a9493ec996a5870cdb2035
Po Chun, Lu <oceanus11034@gmail.com>
2017-12-26 22:36:16 +0800

    [Add] card pool sample

--- src/assets/js/data/cardData.js
+++ src/assets/js/data/cardData.js
@@ -1,34 +1,38 @@
 export default {
   1: {
     id: 1,
+    title: 'Mosquito',
     size: {
       medium: 'http://fakeimg.pl/400x400/',
-      defaultSize: 'medium',
     },
+    defaultSize: 'medium',
     link: '#',
   },
   2: {
     id: 2,
+    title: 'Path',
     size: {
       large: 'http://fakeimg.pl/600x200/',
-      defaultSize: 'large',
     },
+    defaultSize: 'large',
     link: '#',
   },
   3: {
     id: 3,
+    title: 'Vote',
     size: {
       medium: 'http://fakeimg.pl/400x600/',
-      defaultSize: 'medium',
     },
+    defaultSize: 'medium',
     link: '#',
   },
   4: {
     id: 4,
+    title: 'Vote',
     size: {
       small: 'http://fakeimg.pl/200x200/',
-      defaultSize: 'small',
     },
+    defaultSize: 'small',
     link: '#',
   },
 };
