1811c56f0e3422ccb128d9e5604b38e43f082622
Po Chun, Lu <oceanus11034@gmail.com>
2017-12-27 23:45:34 +0800

    [Add] Toogle add card buttons list

--- src/assets/js/toggle.js
+++ src/assets/js/toggle.js
@@ -4,5 +4,14 @@ import $ from 'jquery';
   $('.avatar').click(() => {
     $('.avatar-pop').toggleClass('pop--active');
   });
+
+  $('.cards-pool--switch button').click(() => {
+    $('.cards-pool').toggleClass('pool--active');
+    $('.cards-pool--switch').toggleClass('switch--active');
+
+    // change switch btn text
+    const btnText = ($('.cards-pool').hasClass('pool--active')) ? 'v' : '+';
+    $('.cards-pool--switch button').text(btnText);
+  });
 }
 )();
