1e1a5553382963f9bdec1c6a0533c955f0582577
Po Chun, Lu <oceanus11034@gmail.com>
2018-01-04 01:11:20 +0800

    [Update] card order

--- src/assets/data/cardOrder.js
+++ src/assets/data/cardOrder.js
@@ -2,9 +2,8 @@ export default {
   order: [
     { id: 1, size: 'medium' },
     { id: 2, size: 'large' },
-    { id: 4, size: 'medium' },
-    { id: 5, size: 'medium' },
     { id: 3, size: 'large' },
+    { id: 5, size: 'medium' },
   ],
-  usedID: [1, 2, 3, 4, 5],
+  usedID: [1, 2, 3, 5],
 };
