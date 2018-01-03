0604255e20f51240de8b4bd3c5056f565fb02f5a
Po Chun, Lu <oceanus11034@gmail.com>
2018-01-03 20:03:04 +0800

    [Add] signup error handling

--- src/assets/js/components/authComponents/auth.js
+++ src/assets/js/components/authComponents/auth.js
@@ -1,17 +1,9 @@
 import $ from 'jquery';
 import './authRender';
 
-const BASE_URL = 'https://smartcampus.csie.ncku.edu.tw/smart_campus';
-
-const HEADERS = {
-  Accept: 'application/json',
-  'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-};
-
-const ValidateFailMsg = 'Form validation fail';
-
-
-// Local Storage Method
+/**
+ * Local Storage Access Method
+ */
 export function setUserData(userData) {
   localStorage.setItem('userData', JSON.stringify(userData));
 }
@@ -26,7 +18,19 @@ export default function getUserData() {
 }
 
 (() => {
+  /**
+   * Constants
+   */
+  const BASE_URL = 'https://smartcampus.csie.ncku.edu.tw/smart_campus';
+  const ValidateFailMsg = 'Form validation fail';
+  /**
+   * Request Method
+   */
   function request(uri, method, payload = null) {
+    const HEADERS = {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    };
     const content = { method, headers: HEADERS }; // credentials: 'include',
     if (payload) {
       content.body = $.param(payload);
@@ -34,20 +38,32 @@ export default function getUserData() {
     return fetch(uri, content);
   }
 
+  async function setDataByContentType(response) {
+    const contentType = {
+      json: 'application/json; charset=utf-8',
+      text: 'text/html; charset=utf-8',
+    };
+    const resContentType = await response.headers.get('content-type');
+    return (resContentType === contentType.json) ? response.json() : response.text();
+  }
+
   async function getDataFromResponse(response) {
-    if (response.status !== 200) {
+    const successStatus = [200, 201];
+    if (!successStatus.includes(response.status)) {
       const errorMsg = await response.text();
       throw new Error(errorMsg);
     } else {
-      return response.json() || response.text();
+      return setDataByContentType(response);
     }
   }
 
   async function authProcess(route, body) {
     try {
       const response = await request(`${BASE_URL}/${route}/`, 'POST', body);
-      const userData = await getDataFromResponse(response);
-      setUserData(userData);
+      const resData = await getDataFromResponse(response);
+      if (typeof (resData) === 'object') {
+        setUserData(resData.data);
+      }
       window.location.reload();
     } catch (error) {
       $(`#errorMsg--${route}`).text(error.message);
@@ -69,6 +85,9 @@ export default function getUserData() {
     window.location.reload();
   }
 
+  /**
+   * Form Method
+   */
   function checkFormValue(form) {
     if (!form.checkValidity()) {
       form.reportValidity();
@@ -88,7 +107,9 @@ export default function getUserData() {
     }, {});
     return formData;
   }
-
+  /**
+   * Bind Auth Click Events
+   */
   $('#btn--login').click((event) => {
     event.preventDefault();
     const formId = 'form--login';
@@ -105,7 +126,5 @@ export default function getUserData() {
     signup(formData);
   });
 
-  $('#btn--logout').click(()=>{
-    logout();
-  });
+  $('#btn--logout').click(logout);
 })();
