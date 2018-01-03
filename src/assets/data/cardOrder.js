4eaaab3721131e37d44e0e06265adf6e53dabe79
Po Chun, Lu <oceanus11034@gmail.com>
2018-01-03 23:19:43 +0800

    [Update] remove zapper in default block

--- src/assets/data/cardOrder.js
+++ src/assets/data/cardOrder.js
@@ -3,8 +3,7 @@ export default {
     { id: 1, size: 'medium' },
     { id: 3, size: 'large' },
     { id: 4, size: 'medium' },
-    { id: 2, size: 'medium' },
     { id: 5, size: 'small' },
   ],
-  usedID: [1, 3, 4, 2, 5],
+  usedID: [1, 3, 4, 5],
 };
